18b90448db406c8cae2b071637aa9bc631923dd4
mohdshahzil <theshahzil@gmail.com>
2023-10-29 14:46:56 +0530

    sidebar-homepage-corrected-and-completed

--- app/components/FilterMenu.tsx
+++ app/components/FilterMenu.tsx
@@ -1,59 +1,30 @@
-'use client'
-import React, { useState } from 'react';
+"use client"
+import React, { useState } from "react";
+import { MdClose, MdMenu } from "react-icons/md";
 
-const FilterMenu: React.FC = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+const FilterMenu = () => {
+  const [isMobile, setIsMobile] = useState(true);
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
+  const toggleMobile = () => {
+    setIsMobile(!isMobile);
   };
 
   return (
-    <div className={`lg:w-1/4 w-full p-4 bg-black bg-opacity-60 text-white ${isMenuOpen ? 'lg:hidden' : 'lg:block'}`}>
-      <div className="block lg:hidden" onClick={toggleMenu}>
-        <svg
-          className="w-6 h-6 fill-current"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-        >
-          {isMenuOpen ? (
-            <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
-          ) : (
-            <path d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z" />
-          )}
-        </svg>
-      </div>
-      <h2 className="text-2xl font-semibold mb-4">Filter By</h2>
-      <div className="mb-4">
-        <label className="block text-lg font-medium">Languages</label>
-        <div className="flex flex-wrap -mx-2">
-          {/* Buttons for languages */}
-          <button className="bg-blue-500 text-white px-4 py-2 rounded-full m-2 focus:outline-none">JavaScript</button>
-          <button className="bg-blue-500 text-white px-4 py-2 rounded-full m-2 focus:outline-none">Python</button>
-          <button className="bg-blue-500 text-white px-4 py-2 rounded-full m-2 focus:outline-none">Java</button>
+    <div>
+      <button
+        className="absolute top-2 right-2 text-white cursor-pointer "
+        onClick={toggleMobile}
+      >
+        {isMobile ? <MdClose size={24} /> : <MdMenu size={24} />}
+      </button>
+      {isMobile && (
+        <div className="fixed top-0 left-0 h-full  w-[20vw]  flex flex-col p-4 shadow-lg bg-black bg-opacity-40">
+          <div className="text-white">shahzil</div>
+          <div className="text-white">shahzil</div>
         </div>
-      </div>
-      <div className="mb-4">
-        <label className="block text-lg font-medium">Domain</label>
-        <div className="flex flex-wrap -mx-2">
-          {/* Buttons for domains */}
-          <button className="bg-blue-500 text-white px-4 py-2 rounded-full m-2 focus:outline-none">Web Development</button>
-          <button className="bg-blue-500 text-white px-4 py-2 rounded-full m-2 focus:outline-none">Data Science</button>
-          <button className="bg-blue-500 text-white px-4 py-2 rounded-full m-2 focus:outline-none">Machine Learning</button>
-        </div>
-      </div>
-      <div className="mb-4">
-        <label className="block text-lg font-medium">Difficulty</label>
-        <div className="flex flex-wrap -mx-2">
-          {/* Buttons for difficulty levels */}
-          <button className="bg-blue-500 text-white px-4 py-2 rounded-full m-2 focus:outline-none">Beginner</button>
-          <button className="bg-blue-500 text-white px-4 py-2 rounded-full m-2 focus:outline-none">Intermediate</button>
-          <button className="bg-blue-500 text-white px-4 py-2 rounded-full m-2 focus:outline-none">Advanced</button>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
 
 export default FilterMenu;
-
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import Searchbar from "../components/searchbar";
 import RepoCard from "../components/card";
@@ -10,42 +11,42 @@ const Page = () => {
   };
 
   return (
-    <main className="h-screen w-screen flex items-center flex-col" style={containerStyle}>
-      <div className="lg:flex mt-12 ml-4">
-        <FilterMenu /> {/* Adjust the width using Tailwind CSS classes */}
-        <div className="lg:w-2/3 ml-28"> {/* Preserve your existing CSS classes */}
-          <Searchbar />
-          <div className="mt-4">
-            <RepoCard
-              repoName="Kanishk"
-              language="Python"
-              description="hi, this is a sample repo"
-              stars={5}
-              forks={50}
-              url="www.exidge.com"
-            />
-          </div>
-          <div className="mt-4">
-            <RepoCard
-              repoName="Kanishk"
-              language="Python"
-              description="hi, this is a sample repo"
-              stars={5}
-              forks={50}
-              url="www.exidge.com"
-            />
-          </div>
-          <div className="mt-4">
-            <RepoCard
-              repoName="Kanishk"
-              language="Python"
-              description="hi, this is a sample repo"
-              stars={5}
-              forks={50}
-              url="www.exidge.com"
-            />
-          </div>
-        </div>
+    <main
+      className="h-screen w-screen flex items-center flex-col"
+      style={containerStyle}
+    >
+      <FilterMenu />
+
+      <Searchbar />
+      <div className="mt-4">
+        <RepoCard
+          repoName="Kanishk"
+          language="Python"
+          description="hi, this is a sample repo"
+          stars={5}
+          forks={50}
+          url="www.exidge.com"
+        />
+      </div>
+      <div className="mt-4">
+        <RepoCard
+          repoName="Kanishk"
+          language="Python"
+          description="hi, this is a sample repo"
+          stars={5}
+          forks={50}
+          url="www.exidge.com"
+        />
+      </div>
+      <div className="mt-4">
+        <RepoCard
+          repoName="Kanishk"
+          language="Python"
+          description="hi, this is a sample repo"
+          stars={5}
+          forks={50}
+          url="www.exidge.com"
+        />
       </div>
     </main>
   );
