1e653d9200b890712cc3a4b9b7323dd3abee2ee8
mohdshahzil <theshahzil@gmail.com>
2024-02-25 21:11:14 +0530

    landing-page-completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 import React from "react";
-import { motion } from "framer-motion";
+
 import { SparklesCore } from "@/components/ui/sparkles";
 import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
+import ArrowSVG from "@/assets/SVG/ArrowSVG";
 const page = () => {
   const words = [
     {
@@ -27,26 +28,35 @@ const page = () => {
     },
   ];
   return (
-    <div className="min-h-screen relative w-full bg-black flex flex-col items-center justify-center overflow-hidden">
-      <div className="w-full absolute inset-0 h-screen">
-        <SparklesCore
-          id="tsparticlesfullpage"
-          background="transparent"
-          minSize={0.6}
-          maxSize={1.4}
-          particleDensity={100}
-          className="w-full h-full"
-          particleColor="#d3d3d3"
-        />
-      </div>
+    <>
+      <div className="min-h-screen relative w-full bg-black flex flex-col items-center justify-center overflow-hidden">
+        <div className="w-full absolute inset-0 h-screen">
+          <SparklesCore
+            id="tsparticlesfullpage"
+            background="transparent"
+            minSize={0.6}
+            maxSize={1.4}
+            particleDensity={100}
+            className="w-full h-full"
+            particleColor="#d3d3d3"
+          />
+        </div>
 
-      <div className="flex flex-col items-center justify-center h-[40rem]  ">
-        <p className="text-neutral-200 text-xs sm:text-base  ">
-          The road to freedom starts from here
-        </p>
-        <TypewriterEffectSmooth words={words} />
+        <div className="flex flex-col items-center justify-center h-[40rem]  ">
+          <p className="text-neutral-200 text-xs sm:text-base  ">
+            The road to freedom starts from here
+          </p>
+          <TypewriterEffectSmooth words={words} />
+          <button className="inline-flex text-xl  z-50 h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-white   transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+            Get Started{" "}
+            <div className="transition-transform transform hover:translate-x-2">
+              {" "}
+              <ArrowSVG></ArrowSVG>
+            </div>
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
