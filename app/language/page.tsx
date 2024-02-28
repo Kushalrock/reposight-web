d17eb23e2e6ed584d5795980aa02ce4a2063acef
mohdshahzil <theshahzil@gmail.com>
2024-02-28 13:47:23 +0530

    language-level-domain-corrected

--- app/language/page.tsx
+++ app/language/page.tsx
@@ -1,14 +1,14 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 import Link from "next/link";
 import { motion } from "framer-motion";
-import useFilterStore from '../Store/store'; 
+import useFilterStore from "../Store/store";
 
 const options: string[] = ["C/C++", "C#", "JS", "Java", "Python", "Other"];
 
 const Page: React.FC = () => {
   const [selectedOptions, setSelectedOptions] = useState<string[]>([]);
-  const setLanguages = useFilterStore(state => state.setLanguages);
+  const setLanguages = useFilterStore((state) => state.setLanguages);
 
   const toggleOption = (option: string) => {
     if (selectedOptions.includes(option)) {
@@ -19,24 +19,24 @@ const Page: React.FC = () => {
   };
 
   const updateStore = () => {
-    console.log(selectedOptions)
+    console.log(selectedOptions);
     setLanguages(selectedOptions);
   };
 
   return (
     <div>
-      <div className="bg-zinc-950 w-screen min-h-screen flex justify-center items-center flex-col text-white">
-        <div className="text-6xl text-center mb-2">
+      <div className=" bg-zinc-950 w-screen min-h-screen flex justify-center items-center flex-col text-white">
+        <div className=" text-6xl text-center mb-2">
           What language do you code in?
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-5 lg:grid-cols-3 gap-10 p-6">
+        <div className="  grid grid-cols-1 md:grid-cols-5 lg:grid-cols-3 gap-10 p-6">
           {options.map((option, index) => (
             <motion.li
               key={index}
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ delay: index * 0.1 }}
-              className={`flex flex-row items-center justify-center px-6 border rounded-full text-2xl cursor-pointer transition duration-300 lg:h-20 h-10 ${
+              className={`flex flex-row items-center justify-center px-6 border rounded-full text-2xl cursor-pointer transition duration-300 lg:h-20 h-14 ${
                 selectedOptions.includes(option)
                   ? "bg-white text-black"
                   : "hover:bg-white hover:text-black"
@@ -47,7 +47,8 @@ const Page: React.FC = () => {
             </motion.li>
           ))}
         </div>
-        <div className="w-2/3 flex flex-row-reverse justify-between">
+     
+        <div className="w-2/3 flex items-center justify-center">
           <Link href="/level">
             <motion.li
               className="bg-white text-black text-2xl m-4  rounded-xl list-none px-8 py-3 flex justify-center items-center cursor-pointer"
