b6a0426e82ec697e8485d897ede0536f0b763033
Aviral Srivastava <81246801+meaviral17@users.noreply.github.com>
2024-03-03 18:18:43 +0530

    Imported Appbar and SparklesCore to Repocard component
    As directed by @mohdshahzil

--- app/components/RepoCard.tsx
+++ app/components/RepoCard.tsx
@@ -8,6 +8,8 @@ import {
   Link,
   Image,
 } from "@nextui-org/react";
+import AppBar from "../components/common/AppBar/AppBar";
+import { SparklesCore } from "@/components/ui/sparkles";
 
 interface RepoCardProps {
   fields: string[];
@@ -17,6 +19,17 @@ const RepoCard: React.FC<RepoCardProps> = ({ fields }) => {
   const [repoName, ownerName, description, githubLink] = fields;
 
   return (
+    <div className="w-full min-h-screen bg-black">
+      <AppBar />
+      <SparklesCore
+        id="tsparticlesfullpage"
+        background="transparent"
+        minSize={0.6}
+        maxSize={1.4}
+        particleDensity={100}
+        className="w-full h-full"
+        particleColor="#FFFFFF"
+    />
     <Card className="w-full p-2 m-2 bg-[#3c3c3c] text-white ">
       <CardHeader className="flex gap-3">
         <div className="flex flex-col">
@@ -47,6 +60,7 @@ const RepoCard: React.FC<RepoCardProps> = ({ fields }) => {
         </div>
       </CardFooter>
     </Card>
+  </div>
   );
 };
 
