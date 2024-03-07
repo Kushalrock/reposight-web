59bdf3f9311657e68a6c7b31893016e7854ca903
Kanishk-S06 <kanishksharma2609@gmail.com>
2024-03-07 22:19:53 +0530

    minor fixes

--- app/components/RepoCard.tsx
+++ app/components/RepoCard.tsx
@@ -1,3 +1,6 @@
+"use client";
+// RepoCard.tsx
+
 import React from "react";
 import {
   Card,
@@ -6,61 +9,44 @@ import {
   CardFooter,
   Divider,
   Link,
-  Image,
 } from "@nextui-org/react";
-import AppBar from "../components/common/AppBar/AppBar";
-import { SparklesCore } from "@/components/ui/sparkles";
-
 interface RepoCardProps {
   fields: string[];
 }
 
 const RepoCard: React.FC<RepoCardProps> = ({ fields }) => {
   const [repoName, ownerName, description, githubLink] = fields;
-
   return (
-    <div className="w-full min-h-screen bg-black">
-      <AppBar />
-      <SparklesCore
-        id="tsparticlesfullpage"
-        background="transparent"
-        minSize={0.6}
-        maxSize={1.4}
-        particleDensity={100}
-        className="w-full h-full"
-        particleColor="#FFFFFF"
-    />
-    <Card className="w-full p-2 m-2 bg-[#3c3c3c] text-white ">
-      <CardHeader className="flex gap-3">
-        <div className="flex flex-col">
-          <p className="text-md">{repoName}</p>
-          <p className="text-small text-default-500">{ownerName}</p>
-        </div>
-      </CardHeader>
-      <Divider className="bg-slate-500 h-[1.5px] " />
-      <CardBody>
-        <p>{description}</p>
-      </CardBody>
-      <div className="flex justify-center"></div>
-      <CardFooter className="flex  flex-col md:flex-row justify-between">
-        <Link
-          isExternal
-          showAnchorIcon
-          href={githubLink}
-          className="text-yellow-400"
-        >
-          Visit source code on GitHub.
-        </Link>
-        <div className="flex flex-row gap-2">
-          <div>Issues</div>
-          <Divider orientation="vertical" className="bg-slate-500  h-auto " />
-          <div>Stars</div>
-          <Divider orientation="vertical" className="bg-slate-500  h-auto " />
-          <div>Forks</div>
-        </div>
-      </CardFooter>
-    </Card>
-  </div>
+      <Card className="w-full p-2 m-2 bg-[#3c3c3c] text-black ">
+        <CardHeader className="flex gap-3">
+          <div className="flex flex-col">
+            <Link href="/issues"><p className="text-md">{repoName}</p></Link>
+            <p className="text-small text-default-500">{ownerName}</p>
+          </div>
+        </CardHeader>
+        <Divider className="bg-slate-500 h-[1.5px] " />
+        <CardBody>
+          <p>{description}</p>
+        </CardBody>
+        <div className="flex justify-center"></div>
+        <CardFooter className="flex  flex-col md:flex-row justify-between">
+          <Link
+            isExternal
+            showAnchorIcon
+            href={githubLink}
+            className="text-yellow-400"
+          >
+            Visit source code on GitHub.
+          </Link>
+          <div className="flex flex-row gap-2">
+            <div>Issues</div>
+            <Divider orientation="vertical" className="bg-slate-500  h-auto " />
+            <div>Stars</div>
+            <Divider orientation="vertical" className="bg-slate-500  h-auto " />
+            <div>Forks</div>
+          </div>
+        </CardFooter>
+      </Card>
   );
 };
 
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -8,73 +8,67 @@ import { motion } from "framer-motion";
 import fetchReposAndIssues from "../Hooks/FetchRepos";
 import useFilterStore from "../Store/store";
 import RepoCard from "../components/RepoCard";
-import { SparklesCore } from "@/components/ui/sparkles";
 
 interface Repo {
   repo_id: string;
   repo_name: string;
   repo_desc: string;
+  repo_url: string; // Include repo_url in Repo interface
 }
 
 const Page: React.FC = () => {
   const [repos, setRepos] = useState<Repo[]>([]);
-  const languages = useFilterStore(state => state.languages);
-  const difficulty = useFilterStore(state => state.difficulties);
-  const topics = useFilterStore(state => state.topics);
+  const languages = useFilterStore((state) => state.languages);
+  const difficulty = useFilterStore((state) => state.difficulties);
+  const topics = useFilterStore((state) => state.topics);
 
   useEffect(() => {
     const fetchRepos = async () => {
       try {
-        const fetchedRepos = await fetchReposAndIssues(languages, difficulty, topics);
-        // Assuming fetchedRepos is an array of JSON objects
+        const fetchedRepos = await fetchReposAndIssues(
+          languages,
+          difficulty,
+          topics
+        );
         const reposData: Repo[] = fetchedRepos.map((repo: any) => ({
           repo_id: repo.repo_id,
           repo_name: repo.repo_name,
-          repo_desc: repo.repo_desc
+          repo_desc: repo.repo_desc,
+          repo_url: repo.repo_url,
         }));
         setRepos(reposData);
       } catch (error) {
         console.error("Error fetching repos:", error);
       }
     };
-    console.log(languages, topics, difficulty);
     fetchRepos();
   }, [languages, difficulty, topics]);
 
   return (
     <div className="w-screen min-h-screen bg-black flex flex-col items-center">
       <AppBar />
-      <SparklesCore
-        id="tsparticlesfullpage"
-        background="transparent"
-        minSize={0.6}
-        maxSize={1.4}
-        particleDensity={100}
-        className="w-full h-full"
-        particleColor="#FFFFFF"
-    />
       <div className="hidden md:block">
         <FilterMenu className="absolute left-6 top-24" />
       </div>
       <div className="md:hidden">
         <MobileFilterMenu></MobileFilterMenu>
       </div>
-      <div className="flex flex-col items-center md:w-2/3 w-4/5 max-h-fit text-white mt-4 ">
-        {repos.map((repo, index) => (
-          <motion.div
-            key={index}
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
-            transition={{ duration: 0.7 }}
-            className="w-full"
-          >
-            <RepoCard
-              key={repo.repo_id}
-              fields={[repo.repo_name, "", repo.repo_desc]}
-            />
-          </motion.div>
-        ))}
-      </div>
+        <div className="flex flex-col items-center md:w-2/3 w-4/5 max-h-fit text-white mt-4 ">
+          {repos.map((repo, index) => (
+            <motion.div
+              key={index}
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              transition={{ duration: 0.7 }}
+              className="w-full"
+            >
+              <RepoCard
+                key={repo.repo_id}
+                fields={[repo.repo_name, "", repo.repo_desc, repo.repo_url]}
+              />
+            </motion.div>
+          ))}
+        </div>
     </div>
   );
 };
