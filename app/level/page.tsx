f71da825bcd246ac62472d574eb603d29d356577
Kanishk Sharma <121635906+Kanishk-S06@users.noreply.github.com>
2024-03-13 23:20:40 +0530

    Update page.tsx
    padding in languages

--- app/level/page.tsx
+++ app/level/page.tsx
@@ -27,7 +27,7 @@ const DifficultyPage: React.FC = () => {
   return (
     <>
       <div className="bg-zinc-950 w-screen min-h-screen flex justify-center items-center flex-col text-white">
-        <div className="text-4xl md:text-6xl text-center mb-2">
+        <div className="text-4xl md:text-6xl text-center mb-2 sm:p-4">
           What type of issues would you like to target?
         </div>
         <div className="grid grid-cols-1 md:grid-cols-5 lg:grid-cols-3 gap-10 p-6">
