1b00fd6b875d4e86b9b260a751d57175798633a6
ManviAgarwal-3 <120966911+ManviAgarwal-3@users.noreply.github.com>
2024-03-14 22:15:03 +0530

    aboutus-page

---
+++ app/aboutus/page.tsx
@@ -0,0 +1,60 @@
+"use client";
+import React from "react";
+import { SparklesCore } from "@/components/ui/sparkles";
+import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
+// import { Button } from "@/components/ui/button";
+import Link from "next/link";
+const page = () => {
+  const words = [
+    {
+      text: "About",
+      className: "text-white  ",
+    },
+    {
+      text: "Us.",
+      className: "text-blue-500 dark:text-blue-500",
+    },
+  ];
+  const word = [
+    {
+      text: "About",
+      className: "text-white  ",
+    },
+    {
+      text: "Us.",
+      className: "text-blue-500 dark:text-blue-500",
+    },
+  ];
+  return (
+    <div className="min-h-screen relative w-full bg-black flex flex-col items-center justify-center overflow-hidden">
+      <div className="w-full absolute inset-0 h-screen">
+        <SparklesCore
+          id="tsparticlesfullpage"
+          background="transparent"
+          minSize={0.6}
+          maxSize={1.4}
+          particleDensity={100}
+          className="w-full h-full"
+          particleColor="#FFFFFF"
+        />
+      </div>
+
+      <TypewriterEffectSmooth
+        
+        words={words}
+      />
+      
+      <div className="flex mt-8" style={{ display: 'flex' }}>
+        <div className="text-white mr-4 flex-grow" style={{ flex: 1 }}>
+          <p className="mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div className="text-white flex-grow" style={{ flex: 1 }}>
+          <p className="mt-4"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default page;
