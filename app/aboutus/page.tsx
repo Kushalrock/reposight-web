c406a5ab5035b8a9ef31470edc8203b0edaaba62
ManviAgarwal-3 <120966911+ManviAgarwal-3@users.noreply.github.com>
2024-03-15 23:50:51 +0530

    commit_2

--- app/aboutus/page.tsx
+++ app/aboutus/page.tsx
@@ -37,21 +37,23 @@ const page = () => {
         <blockquote className="text-center italic text-gray-400 flex flex-col items-center justify-center">"Lorem ipsum dolor sit amet, consectetur adipiscing elit."</blockquote>
     </div>
     <div className="flex mt-8 flex-col sm:flex-row">
-        <div className="text-white mr-4 flex-col sm:w-1/2" style={{ flex: 1, fontSize: '1.5rem', maxWidth: "600px" }}>
+        <div className="text-white mr-4 flex-col sm:w-1/2" style={{ flex: 1, fontSize: '1.0rem', maxWidth: "600px" }}>
             <p className="mt-4" style={{ padding: '0 60px' }}>Lorem ipsum dolor sit amet, ididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
         </div>
-        <div className="text-white mr-4 flex-col sm:w-1/2" style={{ flex: 1, fontSize: '2rem', maxWidth: "600px" }}>
-            <p className="mt-4 flex-col" style={{ padding: '0 120px' }}> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <div className="text-white mr-4 flex-col sm:w-1/2" style={{ flex: 1, fontSize: '2.0rem', maxWidth: "600px" }}>
+            <p className="mt-4 flex-col text-xl" style={{ padding: '0 120px'}}> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </div>
     </div>  
     <style jsx>{`
     @media only screen and (max-width: 768px) {
             .text-white p {
-                font-size: 0.8rem;
+                font-size: 0.6rem;
             }
-
+            .text-white p.mt-4.text-xl {
+                font-size: 1.0rem;
+            }
             blockquote{
-                font-size: 0.8rem;
+                font-size: 0.6rem;
             }
         }
     `}</style>
