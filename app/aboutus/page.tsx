615523bdd57579eb583291b55592a1162a31d35d
ManviAgarwal-3 <120966911+ManviAgarwal-3@users.noreply.github.com>
2024-03-15 23:34:39 +0530

    aboutuscommit

--- app/aboutus/page.tsx
+++ app/aboutus/page.tsx
@@ -2,20 +2,10 @@
 import React from "react";
 import { SparklesCore } from "@/components/ui/sparkles";
 import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
-// import { Button } from "@/components/ui/button";
 import Link from "next/link";
+
 const page = () => {
   const words = [
-    {
-      text: "About",
-      className: "text-white  ",
-    },
-    {
-      text: "Us.",
-      className: "text-blue-500 dark:text-blue-500",
-    },
-  ];
-  const word = [
     {
       text: "About",
       className: "text-white  ",
@@ -43,17 +33,30 @@ const page = () => {
         
         words={words}
       />
-      
-      <div className="flex mt-8" style={{ display: 'flex' }}>
-        <div className="text-white mr-4 flex-grow" style={{ flex: 1 }}>
-          <p className="mt-4">Lorem ipsum dolor sit amet, ididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
-        <div className="text-white flex-grow" style={{ flex: 1 }}>
-          <p className="mt-4"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
-      </div>
-
+    <div className="text-white mt-8 px-4 flex flex-col items-center justify-center" style={{ display: 'flex' }}>
+        <blockquote className="text-center italic text-gray-400 flex flex-col items-center justify-center">"Lorem ipsum dolor sit amet, consectetur adipiscing elit."</blockquote>
     </div>
+    <div className="flex mt-8 flex-col sm:flex-row">
+        <div className="text-white mr-4 flex-col sm:w-1/2" style={{ flex: 1, fontSize: '1.5rem', maxWidth: "600px" }}>
+            <p className="mt-4" style={{ padding: '0 60px' }}>Lorem ipsum dolor sit amet, ididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        </div>
+        <div className="text-white mr-4 flex-col sm:w-1/2" style={{ flex: 1, fontSize: '2rem', maxWidth: "600px" }}>
+            <p className="mt-4 flex-col" style={{ padding: '0 120px' }}> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+    </div>  
+    <style jsx>{`
+    @media only screen and (max-width: 768px) {
+            .text-white p {
+                font-size: 0.8rem;
+            }
+
+            blockquote{
+                font-size: 0.8rem;
+            }
+        }
+    `}</style>
+    </div>
+     
   );
 };
 
