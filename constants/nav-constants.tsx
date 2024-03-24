ad5ce0e2f7b88b15854b5848602be0bd5a518870
Aviral Srivastava <81246801+meaviral17@users.noreply.github.com>
2024-03-24 11:23:03 +0530

    Update nav-constants.tsx

--- constants/nav-constants.tsx
+++ constants/nav-constants.tsx
@@ -1,7 +1,12 @@
 export const navLinks = [
-  { id: "about", title: "About", href: "/about" },
-  { id: "privacyPolicy", title: "Privacy Policy", href: "/privacyPolicy" },
-  ];
-
-
-  
+  { 
+    id: "about", 
+    title: "About", 
+    href: "/about" 
+  },
+  { 
+    id: "privacyPolicy", 
+    title: "Privacy Policy", 
+    href: "/privacyPolicy" 
+  },
+];
