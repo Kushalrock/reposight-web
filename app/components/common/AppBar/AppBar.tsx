7f1ce1f94bae6c89366d24ec1e32ec2b4fe359cc
Aviral Srivastava <81246801+meaviral17@users.noreply.github.com>
2024-03-24 22:33:27 +0530

    toggle menu added

--- app/components/common/AppBar/AppBar.tsx
+++ app/components/common/AppBar/AppBar.tsx
@@ -1,45 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Navbar,
   NavbarBrand,
   NavbarContent,
   NavbarItem,
   Link,
-  Button, 
-  Image
+  Image,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
 } from "@nextui-org/react";
-
 import { navLinks } from "@/constants/nav-constants";
+
 const AppBar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <Navbar
-      isBordered
-      shouldHideOnScroll
-      className="bg-transparent text-white "
-      maxWidth="full"
-    >
-      <NavbarContent justify="end">
-        <NavbarBrand className="mr-4">
-          <Image
-            src="reposight.svg"
-            alt="Logo"
-            width={200}
-            height={40}
-          />
+    <Navbar isBordered shouldHideOnScroll className="bg-black text-white" maxWidth="full">
+      <NavbarContent justify="start" className="sm:hidden">
+        <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} onClick={toggleMenu} />
+      </NavbarContent>
+
+      <NavbarContent className="sm:hidden pr-3" justify="center">
+        <NavbarBrand>
+          <Image src="reposight.svg" alt="Logo" width={200} height={40} />
         </NavbarBrand>
       </NavbarContent>
 
-      <NavbarContent as="div" className="items-center" justify="end">
-  {navLinks.map((nav, index) => (
-    <NavbarItem key={nav.id}>
-      <Link href={nav.href} className="text-white bg-black p-3 rounded-lg text-1.5xl">{nav.title}</Link>
-    </NavbarItem>
-  ))}
-</NavbarContent>
+      <NavbarContent className="hidden sm:flex gap-4" justify="end">
+        <NavbarBrand>
+          <Image src="reposight.svg" alt="Logo" width={200} height={40} />
+        </NavbarBrand>
+        {navLinks.map((nav) => (
+          <NavbarItem key={nav.id}>
+            <Link href={nav.href} className="text-white bg-black p-3 rounded-lg text-2xl">
+              {nav.title}
+            </Link>
+          </NavbarItem>
+        ))}
+      </NavbarContent>
 
-
-
-
+      <NavbarMenu isOpen={isMenuOpen} className="bg-black">
+        {navLinks.map((nav) => (
+          <NavbarMenuItem key={nav.id}>
+            <Link
+              className="text-white bg-black p-3 rounded-lg text-1.5xl"
+              href={nav.href}
+              size="lg"
+            >
+              {nav.title}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
     </Navbar>
   );
 };
